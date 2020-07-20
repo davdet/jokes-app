9c4c965e6551c64d65e63737ead9e4e09965ecd8
dev-dd <dv.dt@hotmail.com>
2020-07-20 14:52:29 +0200

    Init add

---
+++ src/test/java/nn/d3k/jokesapp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package nn.d3k.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
